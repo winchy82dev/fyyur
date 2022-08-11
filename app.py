068f97ba511745d993f53566e9ff249c3f11282f
winchy82dev <nlahkim82@gmail.com>
2022-08-11 05:46:57 +0200

    db Migrate Artist Table

--- app.py
+++ app.py
@@ -51,7 +51,24 @@ class Venue(db.Model):
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
+#class Artist(db.Model):
+    __tablename__ = 'artist'
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
+    
+    def __repr__(self):
+        return f'<Artist {self.id} {self.name}>'
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
