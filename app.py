ebdd8c679724d876daee4eafcf5b912dd5332492
winchy82dev <nlahkim82@gmail.com>
2022-08-11 05:25:32 +0200

    implement Venue DB Initial migration

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,15 +21,16 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# TODO: connect to a local postgresql database
+# connect to a local postgresql database
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venue'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -39,9 +41,17 @@ class Venue(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
+    genres = db.Column(db.ARRAY(db.String()))
+    website_link = db.Column(db.String(255))
+    seeking_description = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<Venue {self.id} {self.name}>' 
+
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
-class Artist(db.Model):
+#class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
