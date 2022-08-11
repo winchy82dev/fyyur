9ec5107a5d20d38c9edab26fdb02bd6040ea8bb6
winchy82dev <nlahkim82@gmail.com>
2022-08-11 04:36:09 +0200

    IMPLEMENT DATABASE URL

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:pop@localhost:5432/todoapp'
